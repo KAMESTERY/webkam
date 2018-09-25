907564d47ee7d49f9c0ca5dd1d76e00cf22e3daf
Derek <derektaylor.us@gmail.com>
2018-09-25 15:50:59 -0500

    Change view name + add text fields

--- ui-src/javascript/app.js
+++ ui-src/javascript/app.js
@@ -7,3 +7,7 @@ console.log('hello style');
 import {MDCRipple} from '@material/ripple';
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
+import {MDCTextField} from '@material/textfield';
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
