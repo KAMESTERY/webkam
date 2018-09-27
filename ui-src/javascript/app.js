66656ffdee77d962b1181a02dfb66577be48ef30
Derek <derektaylor.us@gmail.com>
2018-09-27 00:02:53 -0500

    Add buttons to login

--- ui-src/javascript/app.js
+++ ui-src/javascript/app.js
@@ -2,12 +2,14 @@
 // import { MDCTopAppBar } from '@material/top-app-bar/index'
 // const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 
-console.log('hello style');
-//
-// import {MDCRipple} from '@material/ripple';
-// const ripple = new MDCRipple(document.querySelector('.foo-button'));
-
+import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
+
+new MDCRipple(document.querySelector('.cancel'));
+new MDCRipple(document.querySelector('.next'));
+
 
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
+
+console.log('Styles loaded');
