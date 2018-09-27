3bcc702a75f07e8d73f57ebbd2c9867bff930b33
Derek <derektaylor.us@gmail.com>
2018-09-26 23:34:19 -0500

    Fix js for text field component

--- ui-src/javascript/app.js
+++ ui-src/javascript/app.js
@@ -1,11 +1,11 @@
 
-import { MDCTopAppBar } from '@material/top-app-bar/index'
-const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+// import { MDCTopAppBar } from '@material/top-app-bar/index'
+// const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 
 console.log('hello style');
-
-import {MDCRipple} from '@material/ripple';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+//
+// import {MDCRipple} from '@material/ripple';
+// const ripple = new MDCRipple(document.querySelector('.foo-button'));
 
 import {MDCTextField} from '@material/textfield';
 
