110567919442111058a3c2a391eff215aae65332
Derek <derektaylor.us@gmail.com>
2018-09-28 22:25:26 -0500

    Add register page

--- ui-src/javascript/app.js
+++ ui-src/javascript/app.js
@@ -1,15 +1,28 @@
 
-// import { MDCTopAppBar } from '@material/top-app-bar/index'
-// const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+import {MDCTopAppBar} from '@material/top-app-bar/index';
 
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+console.log('Starting import');
 import {MDCRipple} from '@material/ripple';
 import {MDCTextField} from '@material/textfield';
 
+// home
+// global
+console.log('Starting load');
+
 new MDCRipple(document.querySelector('.cancel'));
-new MDCRipple(document.querySelector('.next'));
-
-
 const username = new MDCTextField(document.querySelector('.username'));
 const password = new MDCTextField(document.querySelector('.password'));
+const confirmPassword = new MDCTextField(document.querySelector('.confirm-password'));
+console.log('Global Styles loaded');
 
-console.log('Styles loaded');
+// login
+new MDCRipple(document.querySelector('.login'));
+console.log('login page Styles loaded');
+
+// register
+new MDCRipple(document.querySelector('.register'));
+console.log('register page Styles loaded');
