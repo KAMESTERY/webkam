10aa3cdaa165ca587473b6c928f6d539020f6941
outcastgeek <outcastgeek+git@gmail.com>
2018-10-12 15:44:34 -0500

    Introduced Flashes

--- endpoints/user.go
+++ endpoints/user.go
@@ -1,6 +1,7 @@
 package endpoints
 
 import (
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 	"github.com/utrack/gin-csrf"
 	"kamestery.com/models"
@@ -16,6 +17,7 @@ func login(c *gin.Context) {
 		"title": "Login",
 	}, "public/login.html")
 }
+
 func register(c *gin.Context) {
 	render(c, gin.H{
 		"csrf": csrf.GetToken(c),
@@ -25,14 +27,18 @@ func register(c *gin.Context) {
 
 func authenticate(c *gin.Context) {
 
+	session := sessions.Default(c)
+
 	var user models.User
 	if err := c.ShouldBind(&user); err != nil {
-		login(c) //TODO: Works but do it better
+		session.AddFlash("Figure out which Error you Made!") //TODO: Revisit this!
+		login_error(c, session.Flashes())
 		return
 	}
 
 	if validationErrors, err := utils.ValidateStruct(&user); validationErrors != nil || err != nil {
-		login(c) //TODO: Works but do it better
+		session.AddFlash("Figure out which Error you Made!") //TODO: Revisit this!
+		login_error(c, session.Flashes())
 		return
 	}
 
@@ -47,3 +53,12 @@ func authenticate(c *gin.Context) {
 
 	c.Redirect(http.StatusFound, "/")
 }
+
+func login_error(c *gin.Context, flashes []interface{}) {
+	render(c, gin.H{
+		"csrf": csrf.GetToken(c),
+		"title": "Login",
+		"flashes": flashes,
+	}, "public/login.html")
+}
+
