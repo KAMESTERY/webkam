e56d2be436c8b806cafc663533700643fd23cc07
outcastgeek <outcastgeek+git@gmail.com>
2019-10-19 17:50:10 -0400

    Fetch and Parse Transit JSON Data

---
+++ src/main/services/uisvc.cljs
@@ -0,0 +1,25 @@
+(ns services.uisvc
+  (:require-macros [cljs.core.async.macros :refer [alt! go]])
+  (:require [cljs.core.async
+             :as    async
+             :refer [<! put! chan close! timeout pipeline-async to-chan]]
+            [taoensso.timbre :as log]
+            [cljs-http.client :as http]
+            [cljs-http.util :as httputil]
+            [cognitect.transit :as t]
+            [bidi.bidi :refer [path-for]]
+            [routing :refer [routing-data]]))
+
+(defn- clj->json
+  [ds]
+  (.stringify js/JSON (clj->js ds)))
+
+(defn <fetch [& params]
+  (go
+    (let [r (t/reader :json)
+          res (<! (http/get
+                   (apply path-for routing-data params)))
+          raw-data (-> res :body clj->json)
+          data (t/read r raw-data)]
+      data)))
+
--- src/main/ui/core.cljs
+++ src/main/ui/core.cljs
@@ -1,10 +1,15 @@
 (ns ui.core
-  (:require [taoensso.timbre :as log]
+  (:require-macros [cljs.core.async.macros :refer [alt! go go-loop]])
+  (:require [cljs.core.async
+             :as    async
+             :refer [<! take! put! chan close! timeout pipeline-async to-chan]]
+            [taoensso.timbre :as log]
             [bidi.bidi :as bidi :refer [path-for]]
             [bidi.router :as br]
             [routing :refer [routing-data]]
             [reagent.core :as r]
             [re-frame.core :as rf :refer [subscribe dispatch]]
+            [services.uisvc :as uisvc :refer [<fetch]]
             [ui.pages.core :as p]))
 
 (enable-console-print!)
@@ -12,24 +17,57 @@
 ;;;; Event Handlers
 
 (rf/reg-event-db
- :page
+ :current-page
  [(when ^boolean goog.DEBUG re-frame.core/debug)]   ;;  <----  conditional!
- (fn [db [_ new-page]]
-   (assoc db :page new-page )))
+ (fn [db [_ current-page-data]]
+   (go
+     (let [{page :page data-chan :data-chan} current-page-data
+           page-data (<! data-chan)]
+       (println "New Page Data: " page-data)
+       (assoc db :page page :page-data @d)))
+   db))
+
+;; (rf/reg-event-db
+;;  :current-page
+;;  [(when ^boolean goog.DEBUG re-frame.core/debug)]   ;;  <----  conditional!
+;;  (fn [db [_ current-page-data]]
+;;    (let [d (atom nil)]
+;;      (go
+;;        (let [{page :page data-chan :data-chan} current-page-data
+;;              page-data (<! data-chan)]
+;;          (println "New Page Data: " page-data)
+;;          (reset! d page-data)))
+;;      (while (nil? d)
+;;        (js/setTimeout (fn []) 10))
+;;      (assoc db :page page :page-data @d))))
+
+;;;; Effects Handlers
+
+(rf/reg-event-fx
+ :render-page
+ [(when ^boolean goog.DEBUG re-frame.core/debug)]   ;;  <----  conditional!
+ (fn [cofx [_ page]]
+   (let [fetch-target (-> (name page)
+                          (str "-json")
+                          keyword)]
+     {:dispatch [:current-page {:page page :data-chan (<fetch fetch-target)}]}
+     )))
 
 ;;;; Subcription Handlers
 
+
 (rf/reg-sub
- :page
+ :current-page
  (fn [db _]
-   (:page db)))
+   (select-keys db [:page :page-data])))
 
 ;;;; Pages
 
 (defmulti page identity)
 
-(defmethod page :home []
-  [p/home []])
+(defmethod page :home [data]
+  (println "Home Data: " data)
+  [p/home data])
 
 (defmethod page :login []
   [p/login []])
@@ -41,23 +79,24 @@
   [p/home []])
 
 ;;;; Routing
-(def router
-  (br/start-router! routing-data
-                    {:default-location {:handler :home}
-                     :on-navigate #(dispatch [:page %])}))
+;; (def router
+;;   (br/start-router! routing-data
+;;                     {:default-location {:handler :home}
+;;                      :on-navigate #(dispatch [:render-page %])}))
 
 ;;;; Rendering
-(defn current-page []
-  (let [activep @(subscribe [:page])]
+(defn current-page [data]
+  (let [{activep :page page-data :page-data} @data]
     (println "Current Page: " activep)
-    (br/set-location! router {:handler activep})
-    (page activep)))
+    ;;(br/set-location! router {:handler activep})
+    [page activep page-data]))
 
 (defn start []
   (do
-    (rf/dispatch-sync [:page :home])
+    (rf/dispatch-sync [:render-page :home])
     (r/render-component
-       [current-page]
-       (.getElementById js/document "main-content"))))
+     [current-page (subscribe [:current-page])]
+     (.getElementById js/document "main-content"))))
 
 (start)
+
